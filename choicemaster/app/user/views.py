0db537571fdab19ebbc78370bd5d3b2dfa5b9ef8
HuairaG <ldarkllarg@gmail.com>
2016-10-23 19:37:42 -0300

    login fixed

--- choicemaster/app/user/views.py
+++ choicemaster/app/user/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 from app.user.forms import RegisterForm
+
 class RegisterUser(CreateView):
     models = User
     template_name = "register.html"
