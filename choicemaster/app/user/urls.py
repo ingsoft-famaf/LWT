f2214c57a7e0e7036fa8856ef3cf591903424826
HuairaG <ldarkllarg@gmail.com>
2016-10-25 09:36:59 -0300

    asd

--- choicemaster/app/user/urls.py
+++ choicemaster/app/user/urls.py
@@ -5,9 +5,7 @@ from django.views.generic.base import TemplateView
 from app.user.views import RegisterUser
 
 urlpatterns = [
-    url(r'^$', auth_views.login, {'template_name': 'login.html', (
-        'redirect_authenticated_user': True}, name='login')),
+    url(r'^$', auth_views.login, {'template_name': 'login.html', 'redirect_authenticated_user': True}, name='login'),
     url(r'^register/', RegisterUser.as_view(), name="register"),
-    url(r'^logout/', auth_views.logout, {'template_name': 'logged_out.html'}, (
-        name='logout')),
+    url(r'^logout/', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
 ]
